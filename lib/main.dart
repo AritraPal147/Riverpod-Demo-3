d86e6b2c25903a7a87f78abf8bcd56d314233d3a
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-23 21:04:45 +0530

    Added fake api for testing

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,37 @@ void main() {
   ));
 }
 
+/// List of cities
+enum City {
+  stockholm,
+  paris,
+  tokyo,
+}
+
+/// [WeatherEmoji] is a naming of [String] to make code more
+/// readable and understandable
+typedef WeatherEmoji = String;
+
+/// This function returns [Future] of [WeatherEmoji] type
+/// corresponding t the [city] that is provided to it
+///
+/// Future.delayed is used so that an api call time can be simulated.
+/// The function returns different emojis based on which city
+/// is provided to it.
+/// If a city is not in below given list, then '?' is returned
+Future<WeatherEmoji> getWeather(City city) {
+  return Future.delayed(
+    const Duration(seconds: 1),
+    () =>
+        {
+          City.stockholm: '❄️',
+          City.paris: '🌧️',
+          City.tokyo: '💨',
+        }[city] ??
+        '?',
+  );
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -20,7 +51,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Weather'),
       ),
     );
   }
